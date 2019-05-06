4ee06ba3e95b0539be1f17ef78afed11e8a1de20
flaviocruz <flaviocoutinho.nave@gmail.com.br>
2019-05-06 18:03:58 -0300

    organizando main

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,21 @@ public class Main {
     public static void main(String[] args){
         Scanner tc = new Scanner(System.in);
 
+        /**
+         * URL example
+         * controller
+         * controller?param=value
+         * controller/method?param=value
+         * controller/method?param1=value&param2=value
+         *
+         * code examples
+         * aluno
+         * aluno?matricula=123456789
+         * aluno/list?matricula=123456789
+         * @param url
+         */
         String url = tc.next();
-
         Parser parser = new Parser(url);
-
-
         Object obj = parser.execute();
-
-        System.out.println();
     }
 }
